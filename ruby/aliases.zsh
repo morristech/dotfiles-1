89f8c14ca2e0193193a816de5144b8a192614112
Peter Rhoades <createdbypete@gmail.com>
2013-07-28 18:47:41 +0100

    Cleaning up files

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,6 +1,2 @@
-alias r='rbenv local 2.0.0-p195'
-
-alias sc='bundle exec rails console'
-alias sg='bundle exec rails generate'
-
+alias r='rbenv local 2.0.0'
 alias migrate='rake db:migrate db:test:prepare'
